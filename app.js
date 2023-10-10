83fac1e82c82557537cb2bfe9df406aded696f51
Monil3571 <monilpatel234@gmail.com>
2023-10-10 18:00:45 -0400

    initial

---
+++ app.js
@@ -0,0 +1,6 @@
+let scaleUp=document.getElementById("scaleUp");
+
+scaleUp.addEventListener("click",()=>{
+    document.getElementById("mountains-svg").style.width="500px";
+    document.getElementById("mountains-png").style.width="500px";
+});
